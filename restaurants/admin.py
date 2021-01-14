c0237190d891abf02db4a6d1c7ac0e523fa5bfb1
Shouvik Patra <shouvikpatra@gmail.com>
2021-01-14 14:47:59 +0530

    Created Restuarant Model

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    exclude = ('id',)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('id', 'res_id')
+    exclude = ('id',)
+
 
 # Register your models here.
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Menu, MenuAdmin)
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    username = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=1500, null=True)
+    contact = models.IntegerField(max_length=10, null=True)
+    maxActiveOrders = models.IntegerField(max_length=100, null=True)
+
+
+class Menu(models.Model):
+    category = (('Chinese', 'Chinese'),
+                ('North Indian', 'North Indian'), ('South Indian',
+                                                   'South Indian'), ('Italian', 'Italian'),
+                )
+    res_id = models.ForeignKey(
+        "restaurants.Restaurant", on_delete=models.CASCADE)
+    dish_name = models.CharField(max_length=255, null=True)
+    price = models.FloatField(max_length=255, null=True)
+    availability = models.BooleanField(default=True)
+    cuisine = models.CharField(max_length=50, choices=category)
